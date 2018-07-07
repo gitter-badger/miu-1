f878da411a28165adfa72a01a1b35900d790852d
theindigamer <theindigamer15@gmail.com>
2018-07-07 04:07:20 -0400

    Add tutorial example.
    LALRPOP is kept as a submodule because the latest version isn't on crates.io

--- miuki/src/main.rs
+++ miuki/src/main.rs
@@ -1,6 +1,18 @@
 extern crate miuki;
 
+#[macro_use] extern crate lalrpop_util;
+
 use miuki::test;
+
+lalrpop_mod!(pub calc); // synthesized by LALRPOP
+
+#[test]
+fn calc() {
+    assert!(calc::TermParser::new().parse("22").is_ok());
+    assert!(calc::TermParser::new().parse("(22)").is_ok());
+    assert!(calc::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calc::TermParser::new().parse("((22)").is_err());
+}
 
 fn main() {
     test();
